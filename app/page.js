7eea91a5921f8bf787a4c1027d4235e632c1c5f5
Karim Guettech <karimguettech@gmail.com>
2023-12-15 12:06:29 +0100

    added sketch widget and point renderer for population

--- app/page.js
+++ app/page.js
@@ -8,18 +8,19 @@ import Graphic from "@arcgis/core/Graphic";
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 import { useEffect, useRef } from "react";
 import Search from "@arcgis/core/widgets/Search.js";
+import Sketch from "@arcgis/core/widgets/Sketch.js";
+import DotDensityRenderer from "@arcgis/core/renderers/DotDensityRenderer";
+import Legend from "@arcgis/core/widgets/Legend";
+import Expand from "@arcgis/core/widgets/Expand";
+import Bookmarks from "@arcgis/core/widgets/Bookmarks";
 
 export default function Home() {
   const mapDiv = useRef(null);
-
   useEffect(() => {
     if (mapDiv.current) {
       /**
        * Initialize application
-       */
-      const graphicsLayer = new GraphicsLayer();
-
-      const labelClass = {
+       */ const labelClass = {
         // autocasts as new LabelClass()
         symbol: {
           type: "text", // autocasts as new TextSymbol()
@@ -42,6 +43,52 @@ export default function Home() {
       const featureLayer = new FeatureLayer({
         url: "https://services.arcgis.com/RL4g2ycRCRI9aQRK/arcgis/rest/services/ComuniItaliani_WFL1/FeatureServer",
         labelingInfo: [labelClass],
+      });
+      const featureLayerP = new FeatureLayer({
+        url: "https://services.arcgis.com/RL4g2ycRCRI9aQRK/arcgis/rest/services/ComuniItaliani_WFL1/FeatureServer",
+        labelingInfo: [labelClass],
+        minScale: 1820000,
+        maxScale: 35000,
+        title: "Popolazione corrente",
+      });
+
+      const graphicsLayer = new GraphicsLayer();
+      const webmap = new Map({
+        basemap: "topo",
+        layers: [featureLayer, featureLayerP],
+      });
+
+      const view = new MapView({
+        container: mapDiv.current, // The id or node representing the DOM element containing the view.
+        map: webmap, // An instance of a Map object to display in the view.
+        center: [11.60849, 44.8653],
+        scale: 100000, // Represents the map scale at the center of the view.
+      });
+
+      view.when().then(() => {
+        const dotDensityRenderer = new DotDensityRenderer({
+          // The number of units per dot
+          dotValue: 3,
+          outline: null,
+          // When referenceScale is set, linearly scales the dot value
+          // based on the referenceDotValue. The layer view will draw
+          // dots using the value in referenceDotValue only at the scale
+          // specified here.
+          referenceScale: 100000,
+          legendOptions: {
+            // Displays "1 dot = 100 people" in the legend
+            unit: "people",
+          },
+          attributes: [
+            {
+              // One red dot will be drawn for every 100 White people
+              field: "Popolazione_residente_al_31_12_",
+              color: "#f23c3f",
+              label: "Popolazione",
+            },
+          ],
+        });
+        featureLayerP.renderer = dotDensityRenderer;
       });
 
       const template = {
@@ -68,7 +115,7 @@ export default function Home() {
         ],
       };
       featureLayer.popupTemplate = template;
-      const point = {
+      /*      const point = {
         //Create a point
         type: "point",
         longitude: 11.60649,
@@ -104,12 +151,9 @@ export default function Home() {
         geometry: polyline,
         symbol: simpleLineSymbol,
       });
+ */
 
-      const webmap = new Map({
-        basemap: "topo",
-        layers: [featureLayer],
-      });
-      const polygon = {
+      /*  const polygon = {
         type: "polygon",
         rings: [
           [11.60649, 44.8657], //Longitude, latitude
@@ -132,28 +176,48 @@ export default function Home() {
       const polygonGraphic = new Graphic({
         geometry: polygon,
         symbol: simpleFillSymbol,
-      });
+      }); */
 
-      const view = new MapView({
-        container: mapDiv.current, // The id or node representing the DOM element containing the view.
-        map: webmap, // An instance of a Map object to display in the view.
-        center: [11.60849, 44.8653],
-        scale: 100000, // Represents the map scale at the center of the view.
-      });
       var searchWidget = new Search({
         view: view,
       });
       view.ui.add(searchWidget, {
         position: "top-right",
       });
-      const pointGraphic = new Graphic({
+      /*   const pointGraphic = new Graphic({
         geometry: point,
         symbol: simpleMarkerSymbol,
-      });
+      }); */
+      view.when(() => {
+        const sketch = new Sketch({
+          layer: graphicsLayer,
+          view: view,
+          // graphic will be selected as soon as it is created
+          creationMode: "update",
+        });
 
-      graphicsLayer.add(pointGraphic);
-      graphicsLayer.add(polylineGraphic);
-      graphicsLayer.add(polygonGraphic);
+        view.ui.add(sketch, "top-right");
+      });
+      /*   graphicsLayer.add(pointGraphic); */
+      /*   graphicsLayer.add(polylineGraphic);
+      graphicsLayer.add(polygonGraphic); */
+      view.ui.add(
+        [
+          new Expand({
+            view: view,
+            content: new Legend({ view: view }),
+            group: "top-left",
+            expanded: true,
+          }),
+          /*   new Expand({
+            view: view,
+            content: new Bookmarks({ view: view }),
+            group: "top-left",
+          }), */
+        ],
+        "bottom-left"
+      );
+
       webmap.add(graphicsLayer);
       // view.navigation.momentumEnabled = false; the map keeps moving after the click of the mouse - enabled by default
       return () => view && view.destroy();
@@ -161,12 +225,11 @@ export default function Home() {
 
     // Add the search widget to the top right corner of the view
   }, []);
-
   return (
     <div
       className="mapDiv"
       ref={mapDiv}
-      style={{ height: "100vh", width: "100%" }}
+      style={{ height: "93vh", width: "100%" }}
     ></div>
   );
 }
